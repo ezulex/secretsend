5274754de0cbc56e6822ad0a0536bc64ec2a05cd
ezulex <bochik1989@mail.ru>
2022-12-08 16:47:14 +0400

    Create project

---
+++ src/main/java/org/ezulex/secretsend/SecretsendApplication.java
@@ -0,0 +1,13 @@
+package org.ezulex.secretsend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecretsendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecretsendApplication.class, args);
+    }
+
+}
